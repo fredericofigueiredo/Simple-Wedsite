f302cc0157127d42c6fc789819956de9220992e4
Frederico Figueiredo <fredericoadf@gmail.com>
2024-01-06 19:01:55 +0100

    All changes for today, new logo, pictures, and beginning of about page.

--- app.py
+++ app.py
@@ -3,36 +3,42 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-#Base URL
+# Base URL
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
-#Subpages
+# Subpages
 @app.route('/home/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/about/')
 def about():
     return render_template('about.html')
 
+
 @app.route('/projects/')
 def projects():
     return render_template('projects.html')
+
 
 @app.route('/resume/')
 def resume():
     return render_template('resume.html')
 
+
 @app.route('/content/')
 def content():
     return render_template('content.html')
 
+
 @app.route('/motd/')
 def meme():
     return render_template('meme.html')
+
 
 @app.route('/contact/')
 def contact():
